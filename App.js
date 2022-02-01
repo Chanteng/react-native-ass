444a5a778e62def7f76773fa7775229246f95fe7
Chanteng <yrnchanteng@gmail.com>
2022-02-01 11:22:24 +0000

    done

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.img} source={require("./assets/2.jpg")} />
+
+        <View style={styles.mother}>
+      <Text style={styles.text}>School</Text>
+      <TextInput style={styles.textinput} placeholder="Codetrain Uni. College" />
+     
+
+      <Text style={styles.text}>Email</Text>
+      <TextInput style={styles.textinput} placeholder="yrnchanteng@gmail.com" />
+      
+     
+      <Text style={styles.text}>Name</Text>
+      <TextInput style={styles.input} placeholder="Oduro Isaac Sapey" underlineColorAndroid={"transparent"} />
+      
+      <Text style={styles.text}>Nickname</Text>
+      <TextInput style={styles.input} placeholder="MR. CEO" underlineColorAndroid={"transparent"} />
+      
+      <Text style={styles.text}>Emergency Contact</Text>
+      <TextInput style={styles.input} placeholder="0570885254" underlineColorAndroid={"transparent"} />
+      
+     
+     
+      </View>
+
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+
+
+const styles = StyleSheet.create( {
+container: {
+    backgroundColor: "orange",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+},
+
+text: {
+    fontSize: 20,
+    color: "black",
+    paddingBottom: 1,
+    marginBottom: 1,
+},
+
+textinput: {
+  alignSelf: "stretch",
+  height: 40,
+  marginBottom: 30,
+ fontSize: 24,
+ color: "white",
+
+},
+
+input: {
+  alignSelf: "stretch",
+  height: 40,
+  color: "white",
+  marginBottom: 30,
+ fontSize: 24,
+ borderBottomColor: "green",
+ borderBottomWidth: 1
+},
+
+
+
+img: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 50
+},
+
+mother: {
+    alignSelf: "center"
+}
+
+})
+
+export default App;
